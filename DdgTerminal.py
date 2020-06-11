bca80c5be006e51cd0766534df11dbb19a3d663a
TheFakeWater <43514781+TheFakeWater@users.noreply.github.com>
2020-06-11 15:50:05 +0200

    Initial Commit

---
+++ DdgTerminal.py
@@ -0,0 +1,28 @@
+#Imports
+from time import sleep
+from selenium import webdriver
+from logger import logger
+
+#Options
+firefoxOptions = webdriver.FirefoxOptions()
+firefoxOptions.headless = False
+
+#Terminal
+print("Hi Welcome to DuckDuckGo Terminal")
+sleep(1)
+search = input("Type your search : ")
+if search == "":
+    while search == "":
+        search = input("Try again : ")
+
+
+
+url = "https://www.duckduckgo.com/?q={}".format(search)
+
+try:
+    driver = webdriver.Firefox(options=firefoxOptions)
+    driver.get(url)
+    logger.info("Success ! ")
+    sleep(1)
+except:
+    logger.critical("Failed to launch the browser")
